b119edbb88535507d6dd6d5e3160348f25e3b7c5
Christoph <cmeinhardt@gmx.at>
2018-06-09 19:35:44 +0200

    trying endless level

---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour {
+
+    public GameObject[] obj;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Spawn(Vector3 position)
+    {
+        Instantiate(obj[Random.Range(0, obj.Length)], position, Quaternion.identity);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ship1" || collision.tag == "Ship2")
+        {
+            Spawn(new Vector3(transform.position.x+25,transform.position.y, transform.position.z));
+            transform.position = new Vector3(transform.position.x + 25, transform.position.y, transform.position.z);
+        }
+    }
+}
