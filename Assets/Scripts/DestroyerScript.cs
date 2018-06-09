37b52d63ac0c8716f1e6f3a5d07b0ead894c3720
Christoph <cmeinhardt@gmx.at>
2018-06-09 20:03:26 +0200

    Endless running and destroying level working

---
+++ Assets/Scripts/DestroyerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyerScript : MonoBehaviour {
+
+    public GameObject ship1;
+    public GameObject ship2;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Mathf.Min(ship1.transform.position.x, ship2.transform.position.x)-100, transform.position.y, transform.position.z);
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
