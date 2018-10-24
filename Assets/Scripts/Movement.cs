0a98febe1773f72881a6e3e018bddb2dc2d733ee
Christoph <cmeinhardt@gmx.at>
2018-10-24 15:50:45 +0200

    Added running Animation for player2

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -150,7 +150,7 @@ public class Movement : MonoBehaviour
             }
             else
             {
-                //anim.SetBool("isRunning", false);
+                anim.SetBool("isRunning", false);
                 DeAccelerate();
             }
 
